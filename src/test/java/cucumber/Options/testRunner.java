8863fd0e526f2edb261df1cede21404b7f35262d
gravicss <Rajveers039@gmail.com>
2023-01-27 11:57:41 +0530

    new test runner class

--- src/test/java/cucumber/Options/testRunner.java
+++ src/test/java/cucumber/Options/testRunner.java
@@ -1,6 +1,12 @@
 package cucumber.Options;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.vimalselvam.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -9,5 +15,12 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\java\\feature\\billPayment.feature", glue= {"stepDefination"})
 public class testRunner {
+	 @AfterClass
+	    public void tearDown() {
+	        Reporter.loadXMLConfig(new File("extent-config.xml"));
+	        Reporter.setSystemInfo("user", System.getProperty("user.name"));
+	        Reporter.setSystemInfo("os", "Windows");
+	        Reporter.setTestRunnerOutput("Sample test runner output message");
+	    }
 
 }
